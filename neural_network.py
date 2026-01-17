7dc21e586217c56ed395bacb7be01a90a7b9d1f6
Muhammad Hamza <156505688+Hamza-Faarooq@users.noreply.github.com>
2026-01-17 15:47:41 +0530

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -6,3 +6,10 @@ model(x)
 
 
 # this is a very basic model, just to get the gist of the basic code utilising pytorch
+## other models used
+nn.Linear
+nn.Conv2d
+nn.LSTM
+nn.Embedding
+nn.LayerNorm
+nn.Dropout
